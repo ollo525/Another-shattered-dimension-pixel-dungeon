c4935954ba5b24cefacd0f04ebc07985476b3b06
ollo525 <153762826+ollo525@users.noreply.github.com>
2026-02-04 15:03:16 +0100

    yeah im vibecoding now beacuse WHAT IS TS, he hallucinated smt, praing it works ;-;

--- SPD-classes/src/main/java/com/watabou/utils/PathFinder.java
+++ SPD-classes/src/main/java/com/watabou/utils/PathFinder.java
@@ -63,12 +63,12 @@ public class PathFinder {
 		maxVal = new int[size];
 		Arrays.fill(maxVal, Integer.MAX_VALUE);
 
-		dir = new int[]{-1, +1, -width, +width, -width-1, -width+1, +width-1, +width+1};
-		dirLR = new int[]{-1-width, -1, -1+width, -width, +width, +1-width, +1, +1+width};
+		dir = new int[]{-1, +1, -width, +width, -1, +1, -width, +width};
+		dirLR = new int[]{-width, -1, -width, -width, +width, +width, +1, +width};
 
 		NEIGHBOURS4 = new int[]{-width, -1, +1, +width};
-		NEIGHBOURS8 = new int[]{-width, -1, +1, +width};
-		NEIGHBOURS9 = new int[]{-width, -1, 0, +1, +width};
+		NEIGHBOURS8 = new int[]{-width, -1, +1, +width, -width, -1, +1, +width};
+		NEIGHBOURS9 = new int[]{-width, -1, 0, +1, +width, -width, -1, 0, +1};
 
 		CIRCLE4 = new int[]{-width, +1, +width, -1};
 		CIRCLE8 = new int[]{-width-1, -width, -width+1, +1, +width+1, +width, +width-1, -1};
