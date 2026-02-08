8c907166337cb773d6db645813495ba3141f4835
ollo525 <153762826+ollo525@users.noreply.github.com>
2026-02-08 20:35:28 +0100

    thats a hallucination, well, i think i need to delete all commits, tf is these names-

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/EmptyBottle.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/EmptyBottle.java
@@ -1,12 +1,13 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.food;
 
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
-import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 import com.watabou.utils.Bundle;
+
 import java.util.ArrayList;
 
 public class EmptyBottle extends Item {
@@ -14,48 +15,51 @@ public class EmptyBottle extends Item {
 	public int durability = 3;
 
 	{
-		image = ItemSpriteSheet.EMPTY_BOTTLE; // Indeks 13
+		image = ItemSpriteSheet.EMPTY_BOTTLE; // Indeks FOOD+13
 		stackable = false;
 	}
 
 	@Override
 	public String name() {
-		return "Empty Bottle (" + durability + "/3)";
+		return Messages.get(this, "name") + " (" + durability + "/3)";
 	}
+
 	@Override
 	public String desc() {
 		return Messages.get(this, "desc");
 	}
 
 	@Override
+	public boolean isIdentified() {
+		return true;
+	}
+
+	@Override
 	public ArrayList<String> actions(Hero hero) {
 		ArrayList<String> actions = super.actions(hero);
-		actions.add("FILL"); // Używamy klucza "FILL"
+		actions.add("FILL");
 		return actions;
 	}
+
 	@Override
 	public void execute(Hero hero, String action) {
-		if (action.equals("FILL")) { // Tu też musi być "FILL"!
-			
+		if (action.equals("FILL")) {
 			if (Dungeon.level.water[hero.pos]) {
 				
-				// USUWANIE SIEBIE
 				detach(hero.belongings.backpack);
 				
-				// SPAWNOWANIE PEŁNEJ
 				WaterBottle water = new WaterBottle();
 				water.durability = this.durability;
 				if (!water.doPickUp(hero)) {
 					Dungeon.level.drop(water, hero.pos).sprite.drop();
 				}
 				
-				GLog.i("You filled the bottle");
+				GLog.i("Napełniłeś butelkę.");
 				hero.spendAndNext(1f);
 				
 			} else {
-				GLog.w("You need to stay in water!");
+				GLog.w("Musisz stać w wodzie!");
 			}
-			
 		} else {
 			super.execute(hero, action);
 		}
@@ -72,18 +76,4 @@ public class EmptyBottle extends Item {
 		super.restoreFromBundle(bundle);
 		durability = bundle.getInt("durability");
 	}
-    	@Override
-	public boolean isIdentified() {
-		return true;
-	}
-    	@Override
-	public boolean isUpgradable() {
-		return false;
-	}
-		@Override
-	public String name() {
-		return Messages.get(this, "name");
-	}
-
-
 }
