cf0531f9810ed6b15dd5126aa5d7ca58848e2a88
ollo525 <153762826+ollo525@users.noreply.github.com>
2026-04-17 12:53:30 +0200

    i see

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/MeleeWeapon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/MeleeWeapon.java
@@ -357,7 +357,7 @@ public int max(int lvl) {
 		int damage = augment.damageFactor(super.damageRoll( owner ));
 
 		if (owner instanceof Hero) {
-			int exStr = ((Hero)owner).(hero.STR() + hero.attSTRBonus) - STRReq();
+			int exStr = ((Hero)owner).(STR() + attSTRBonus) - STRReq();
 			if (exStr > 0) {
 				damage += Hero.heroDamageIntRange( 0, exStr );
 			}
